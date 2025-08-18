68f3fb5cdf86dfbb85b9185c253aa2d35c18b564
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2025-08-18 16:19:34 +0200

    Introduce VisitDto #9
    This PR adds VisitDto to be used instead of the Visit entity in the VisitController REST API
    
    FAIL_TO_PASS: org.springframework.samples.petclinic.owner.VisitDtoSubmissionTest

--- src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetValidator.java
@@ -47,7 +47,7 @@ public class PetValidator implements Validator {
 			errors.rejectValue("type", REQUIRED, REQUIRED);
 		}
 
-		// birth date validation
+		// birthdate validation
 		if (pet.getBirthDate() == null) {
 			errors.rejectValue("birthDate", REQUIRED, REQUIRED);
 		}
